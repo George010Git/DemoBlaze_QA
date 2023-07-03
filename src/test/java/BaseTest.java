5141c8399da273f0d3fa328255c55e8408ddd716
MocanuGeorge010 <mocanu.george010@gmail.com>
2023-07-04 00:47:17 +0300

    Initial Jenkins pipeline update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,7 +18,7 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get(Constants.URL);
     }
-    @AfterTest
+    @AfterMethod
     public void afterScenario() {
         driver.close();
 
